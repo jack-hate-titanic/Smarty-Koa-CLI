db50045142ee8ac13aac3ee2f1cf0ed957095d7c
悦者生存 <1002783067@qq.com>
2023-01-15 12:50:56 +0800

    fix: edit core content

---
+++ packages/core/lib/index.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: 悦者生存 1002783067@qq.com
+ * @Date: 2023-01-15 11:53:25
+ * @LastEditors: 悦者生存 1002783067@qq.com
+ * @LastEditTime: 2023-01-15 12:50:33
+ * @FilePath: /wson-koa2-cli/packages/core/lib/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+'use strict';
+
+module.exports = core;
+
+function core() {
+    return "this is core";
+}
