606fb4befd8440ca665fbdfd9167c1d2c2274307
悦者生存 <1002783067@qq.com>
2023-01-15 13:00:19 +0800

    fix: edit core content

---
+++ packages/core/bin/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: 悦者生存 1002783067@qq.com
+ * @Date: 2023-01-15 12:58:54
+ * @LastEditors: 悦者生存 1002783067@qq.com
+ * @LastEditTime: 2023-01-15 12:59:34
+ * @FilePath: /wson-koa2-cli/packages/core/bin/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#! /usr/bin/env node
+console.log('hello wsondq');
